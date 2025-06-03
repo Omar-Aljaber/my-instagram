329727c4b09072c4a5f36656ee7704ac2465859b
Omar.Aljaber <omar.aljaber@proplant-mvi.com>
2025-06-03 23:49:54 +0200

    Build models and seed data using Model Factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'discrription' => fake()->sentence(),
+            'slug' => fake()->regexify('[A-Za-z0-9]{10}'),
+            'user_id' => User::factory(),
+            'image' => 'https://picsum.photos/seed/' . fake()->unique()->numberBetween(1, 1000) . '/640/480',
+            'likes' => fake()->numberBetween(0, 1000),
+        ];
+    }
+}
